103aa305252808ea109cf6f679a3345e9d4ae086
smpollard0 <spollard301@gmail.com>
2023-06-14 09:48:49 -0700

    add stary background to all pages

--- main.js
+++ main.js
@@ -31,3 +31,12 @@ addButton.addEventListener('click', () => {
   // Clear the input field
   todoInput.value = '';
 });
+
+// Generate stars
+for (let i = 0; i < 100; i++) {
+  const star = document.createElement('div');
+  star.className = 'star';
+  star.style.top = `${Math.random() * 100}vh`;
+  star.style.left = `${Math.random() * 100}vw`;
+  document.body.appendChild(star);
+}
