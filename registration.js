c9c4abd08ef7f4b7b18803685f55db50f549edfc
smpollard0 <spollard301@gmail.com>
2023-06-14 08:37:39 -0700

    add password criteria for more secure passwords

---
+++ registration.js
@@ -0,0 +1,22 @@
+function isValidPassword(password){
+    pattern = /([A-Z].*[a-z]|[a-z].*[A-Z])/gm
+    return pattern.test(password);
+}
+
+function checkPassword(){
+    var password = document.getElementById("password").value;
+    var passwordConfirmation = document.getElementById("password_confirmation").value;
+
+    if (password === "" || passwordConfirmation === "") {
+        document.getElementById("error").innerHTML = "Must enter password.";
+    }// if
+    else if (password !== passwordConfirmation){
+        document.getElementById("error").innerHTML = "Passwords do not match.";
+    }// else if
+    else if (!isValidPassword(password)){
+        document.getElementById("error").innerHTML = "Password must contain upper and lower case characters.";
+    }// else if 
+    else {
+        document.getElementById("error").innerHTML = "Congrats, you don't suck toes.";
+    }// else
+}
