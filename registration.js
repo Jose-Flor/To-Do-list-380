8a2e7646a446eec4a2648d102e557511b901b890
smpollard0 <spollard301@gmail.com>
2023-06-16 18:26:16 -0700

    change location of registration success code

--- registration.js
+++ registration.js
@@ -31,53 +31,54 @@ window.addEventListener("DOMContentLoaded", (event) => {
             document.getElementById("error").innerHTML = "Password must contain upper and lower case characters.";
         }// else if 
         else {
-            document.getElementById("error").innerHTML = "Registration success";
-        }// else
+            const firebaseConfig = {
+                apiKey: "AIzaSyDbI8T-JIXsDn7ty7uSUoF27gzhHxrg4Vo",
+                authDomain: "comp380-2227b.firebaseapp.com",
+                projectId: "comp380-2227b",
+                storageBucket: "comp380-2227b.appspot.com",
+                messagingSenderId: "373668540147",
+                appId: "1:373668540147:web:043bff9b5e81d65b9e9186",
+                measurementId: "G-NB7RQTTWE1"
+            };
+            
+            // Initialize Firebase
+            const app = initializeApp(firebaseConfig);
+            const analytics = getAnalytics(app);
+            
+            //Variable to access database collection
+            const db = getFirestore(app);
+            const dbFormData = collection(db, 'formData');
+            
+            //Get Submit Form
+            let submitButton = document.getElementById("submit");
+            
+            //Create Event Listener To Allow Form Submission
+            submitButton.addEventListener("click", (e) => {
+              //Prevent Default Form Submission Behavior
+              e.preventDefault();
+            
+              //Get Form Values
+              let form_email = document.getElementById("email").value;
+              let form_password = document.getElementById("password").value;
+                
+              // create data package
+              const data = {
+                email: form_email,
+                password: form_password,
+              };
+              
+              //Save Form Data To Firebase
+              addDoc(dbFormData, data)
+              .then(docRef => {
+                console.log(docRef.id); //p4eZcO5QV43IYnigxALJ
+                document.getElementById("error").innerHTML = "Registration success";
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+            });
 
-        const firebaseConfig = {
-            apiKey: "AIzaSyDbI8T-JIXsDn7ty7uSUoF27gzhHxrg4Vo",
-            authDomain: "comp380-2227b.firebaseapp.com",
-            projectId: "comp380-2227b",
-            storageBucket: "comp380-2227b.appspot.com",
-            messagingSenderId: "373668540147",
-            appId: "1:373668540147:web:043bff9b5e81d65b9e9186",
-            measurementId: "G-NB7RQTTWE1"
-        };
-        
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
-        
-        //Variable to access database collection
-        const db = getFirestore(app);
-        const dbFormData = collection(db, 'formData');
-        
-        //Get Submit Form
-        let submitButton = document.getElementById("submit");
-        
-        //Create Event Listener To Allow Form Submission
-        submitButton.addEventListener("click", (e) => {
-          //Prevent Default Form Submission Behavior
-          e.preventDefault();
-        
-          //Get Form Values
-          let form_email = document.getElementById("email").value;
-          let form_password = document.getElementById("password").value;
-        
-          const data = {
-            email: form_email,
-            password: form_password,
-          };
-          
-          //Save Form Data To Firebase
-          addDoc(dbFormData, data)
-          .then(docRef => {
-            console.log(docRef.id); //p4eZcO5QV43IYnigxALJ
-            })
-            .catch(error => {
-                console.log(error);
-            })
-        });
+        }// else
     });
 });
 
