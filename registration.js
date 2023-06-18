70d8585b95d74fb659d4219c6f43ec695d3f8f38
Salvador Vargas <salvadorvargasr7@gmail.com>
2023-06-18 08:51:42 -0700

    Redirection now working

--- registration.js
+++ registration.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getFirestore, collection, addDoc, getCountFromServer, query, where} from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
+import { getFirestore, collection, addDoc, query, where, getDocs } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 function isValidPassword(password){
@@ -20,97 +20,81 @@ const firebaseConfig = {
     measurementId: "G-NB7RQTTWE1"
 };
 
-function createUser(){
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    const auth = getAuth();
-    const dbFormData = collection(db, 'formData');
-    
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const dbFormData = collection(db, 'formData');
+
+async function createUser(form_email, form_password) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        form_email,
+        form_password
+      );
+      // Signed in
+      const user = userCredential.user;
+      // Redirect to signin.html
+      window.location.href = "./signin.html";
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // Show error message in console or on screen
+      console.log(errorCode, errorMessage);
+      document.getElementById("error").innerHTML = "Error during account creation.";
+    }
+}
+
+async function addUserToDB() {
     //Get Form Values
     let form_email = document.getElementById("email").value;
     let form_password = document.getElementById("password").value;
-    
-    // create data package
-    const data = {
-    email: form_email,
-    password: form_password,
-    };
-
-    createUserWithEmailAndPassword(auth, form_email, form_password)
-    .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        // Redirect to signin.html
-        window.location.href = "./signin.html";
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // Show error message in console or on screen
-        console.log(errorCode, errorMessage);
-    });
-}
-
-async function addUserToDB(){
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-    const dbFormData = collection(db, 'formData');
-    
-    //Get Form Values
-    let form_email = document.getElementById("email").value;
-    
-    // create data package
-    const data = {
-    email: form_email,
-    };
 
     // Check if email already exists
     const formQuery = query(
         dbFormData,
         where('email', '==', form_email)
-    )
+    );
 
-    const snapshot = await getCountFromServer(formQuery);
-    const count = snapshot.data().count;
+    const snapshot = await getDocs(formQuery);
+    const count = snapshot.size;
     console.log(count);
 
     if (count > 0){
-        alert("Account associated with that email already exists");
-    }// if
-    else{
-        addDoc(dbFormData, data)
+        document.getElementById("error").innerHTML = "Account associated with that email already exists.";
+    } else {
+        // Create user and add it to firebase auth
+        createUser(form_email, form_password);
+
+        // Add to firestore database
+        addDoc(dbFormData, {email: form_email})
         .then(docRef => {
-        console.log(docRef.id); //p4eZcO5QV43IYnigxALJ
-        document.getElementById("error").innerHTML = "Registration success";
-        createUser();
-        alert("Account creation success!");
+            console.log(docRef.id); //p4eZcO5QV43IYnigxALJ
+            document.getElementById("error").innerHTML = "Registration success";
+            alert("Account creation success!");
         })
         .catch(error => {
             console.log(error);
-        })
-    }// else
+        });
+    }
 }
 
 document.getElementById('submit').addEventListener('click', (e) => {
+    e.preventDefault();
+
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var passwordConfirmation = document.getElementById("password_confirmation").value;
 
     if (!isValidEmail(email)){
         document.getElementById("error").innerHTML = "Invalid email";
-    }// if
-    else if (password === "" || passwordConfirmation === "") {
+    } else if (password === "" || passwordConfirmation === "") {
         document.getElementById("error").innerHTML = "Must enter password.";
-    }// else if
-    else if (password !== passwordConfirmation){
+    } else if (password !== passwordConfirmation){
         document.getElementById("error").innerHTML = "Passwords do not match.";
-    }// else if
-    else if (!isValidPassword(password)){
+    } else if (!isValidPassword(password)){
         document.getElementById("error").innerHTML = "Password must contain upper and lower case characters.";
-    }// else if 
-    else {
-        // add user to database and create user in authentication
+    } else {
         addUserToDB();
-        //document.location.href = "./main.html";
-    }// else
+    }
 });
