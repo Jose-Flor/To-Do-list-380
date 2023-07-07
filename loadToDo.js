dd08abb05e83d6c18f7daa1b9b3e3547ed27abab
Jose Flores <floresjose6829@gmail.com>
2023-07-07 14:20:49 -0700

    random css changes

--- loadToDo.js
+++ loadToDo.js
@@ -40,9 +40,8 @@ async function loadList() {
                     // Clear the input field
                     todoInput.value = '';
                     // Add delete functionality to the button
-                    deleteButton.textContent = 'X';
+                    deleteButton.innerHTML = 'X'; 
                     deleteButton.classList.add('deleteButton'); // assign a class to the button for styling
-
                     // Add text to the list item
                     listItem.textContent = docSnap.data()["task"][i];
                     listItem.appendChild(deleteButton);
@@ -86,5 +85,35 @@ async function loadList() {
         }
       });
 }
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circle");
+
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#e36e5c",
+  "#df685c",
+  "#d5585c",
+  "#d1525c",
+  "#c5415d",
+  "#c03b5d",
+  "#b22c5e",
+  "#ac265e",
+  "#9c155f",
+  "#950f5f",
+  "#830060",
+  "#7c0060",
+  "#680060",
+  "#60005f",
+  "#48005f",
+  "#3d005e"
+];
+
+
+
 
 window.onload = loadList();
