bf00fad08aef3849f577b33a1f5b037ab3a23b4a
smpollard0 <spollard301@gmail.com>
2023-07-09 14:35:15 -0700

    create functioning signout button for teammembers.html

---
+++ teammembers.js
@@ -0,0 +1,47 @@
+import { initializeApp, getApps, deleteApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore, collection, updateDoc, arrayUnion, arrayRemove, doc } from 'https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js';
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { FBConfig } from "./FirebaseConfig.js"
+
+const firebaseConfig = FBConfig();
+
+// Get HTML elements
+const signoutButton = document.getElementById('signout-button'); // Added sign out button
+
+// initialize firebase
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0]; // if already initialized, use that one
+}
+const auth = getAuth();
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, (user) => {
+  // if user is signed in, remove task from their document
+  if (!user) {
+    alert("User is signed out");
+    document.location.href = "./signin.html";
+  }
+});
+
+// Sign Out functionality
+signoutButton.addEventListener('click', () => {
+  signOut(auth).then(() => {
+    // Sign-out successful.
+    document.location.href = "./signin.html";
+  }).catch((error) => {
+    // An error happened.
+    console.error("Error signing out: ", error);
+  });
+});
+
+// Generate stars
+for (let i = 0; i < 100; i++) {
+  const star = document.createElement('div');
+  star.className = 'star';
+  star.style.top = `${Math.random() * 100}vh`;
+  star.style.left = `${Math.random() * 100}vw`;
+  document.body.appendChild(star);
+}
