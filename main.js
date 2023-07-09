2715875b5eb7ad40e931bd08340961afb7e726dd
smpollard0 <spollard301@gmail.com>
2023-07-09 13:56:12 -0700

    centre sign in relative to screen. centre to-do list relative to div

--- main.js
+++ main.js
@@ -84,6 +84,67 @@ addButton.addEventListener('click', () => {
   });
 });
 
+// Add item to the list  for enter key press
+todoInput.addEventListener('keypress', function (e) {
+  if (e.key == 'Enter'){
+    // If input field is empty, then show an alert and stop execution
+    if (!todoInput.value.trim()) {
+      alert("Please enter a task before pressing the 'Add' button.");
+      return;
+    }
+
+    const listItem = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    // Add delete functionality to the button
+    deleteButton.textContent = 'X';
+    deleteButton.classList.add('deleteButton'); // assign a class to the button for styling
+
+    // when delete button is pressed
+    deleteButton.addEventListener('click', () => {
+      onAuthStateChanged(auth, (user) => {
+        // if user is signed in, remove task from their document
+        if (user) {
+          const email = user.email; // get current user's email
+          const dbFormData = collection(db, 'formData'); // collection reference
+          const docReference = doc(db, "formData", email); // document refernce; database, collection, document id
+
+          // remove the task from the user's firebase document
+          updateDoc(docReference, {
+            task: arrayRemove(listItem.innerText.substring(0,listItem.innerText.length-1))
+          }); 
+        }
+      });
+
+      // graphically remove the item from the to-do list
+      todoList.removeChild(listItem);
+    });
+
+    // Add text to the list item
+    listItem.textContent = todoInput.value;
+    listItem.appendChild(deleteButton);
+    todoList.appendChild(listItem);
+
+    // Clear the input field
+    todoInput.value = '';
+
+    onAuthStateChanged(auth, (user) => {
+      // if user is signed in, add task to their document
+      if (user) {
+        const email = user.email; // get current user's email
+        const dbFormData = collection(db, 'formData'); // collection reference
+        const docReference = doc(db, "formData", email); // document refernce; database, collection, document id
+
+        // add the task to the user's firebase document
+        updateDoc(docReference, {
+          task: arrayUnion(listItem.textContent.substring(0,listItem.textContent.length-1))        
+        }); 
+      }
+    });
+  }
+
+});
+
 // Sign Out functionality
 signoutButton.addEventListener('click', () => {
   signOut(auth).then(() => {
